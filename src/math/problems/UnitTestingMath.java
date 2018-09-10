fff43dbb6db68c469b68a9aba42a530dcf7e6a3c
lcabrerav <luisjoelcabrera@gmail.com>
2018-09-10 13:12:26 -0400

    more work for you mr mafi

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,15 +1,10 @@
 package math.problems;
 
+import org.testng.annotations.Test;
+
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
-
-        Factorial factorial = new Factorial();
-        Fibonacci fibonacci = new Fibonacci();
-        FindMissingNumber findMissingNumber =  new FindMissingNumber();
-        LowestNumber lowestNumber = new LowestNumber();
-        MakePyramid makePyramid = new MakePyramid();
-        Pattern pattern= new Pattern();
 
 
 
