f36fec17c69842963104a9194100a73e2665eeef
lcabrerav <luisjoelcabrera@gmail.com>
2018-09-10 18:59:56 -0400

    working on algorithm

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -22,7 +22,7 @@ public class Numbers {
 
 	public static void main(String[] args) throws Exception {
 		
-		int [] num = new int[1000000];
+		int [] num = new int[10000];
 		storeRandomNumbers(num);
 		ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
 		//Selection Sort
@@ -39,25 +39,28 @@ public class Numbers {
 		algo.insertionSort(num);
 		long insertionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
-
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
 		//By following above, Continue for rest of the Sorting Algorithm....
          algo.insertionSort(num);
          long bubbleSortExecutionTime = algo.executionTime;
 		System.out.println("the total execution time" + num.length + "numbers in bubble sort takes : " + bubbleSortExecutionTime
 		 + "mili secs");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
 
-        algo.quickSort(num, findLow(num), findHigh(num));
+		algo.quickSort(num, findLow(num), findHigh(num));
         long quickSortExecutionTime = algo.executionTime;
 		System.out.println("total execution time " + num.length + " numbers in quick Sort takes:" + quickSortExecutionTime + " mili sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
 
         algo.heapSort(num);
         long heapSortExecutationTime = algo.executionTime;
 		System.out.println("total execution time " + num.length + " numbers in heap sort takes " + heapSortExecutationTime+ " mili sec");
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
 
 		algo.bubbleSort(num);
 		long bucketSortExcecutionTime  = algo.executionTime;
 		System.out.println( "total execution time "+ num.length + "number in bucket sort takes " + bucketSortExcecutionTime + " mili sec");
-
+		connectToSqlDB.insertDataFromArrayToSqlTable(num, "selection_sort", "SortingNumbers");
 		//Come to conclusion about which Sorting Algo is better in given data set.
 
 	}
