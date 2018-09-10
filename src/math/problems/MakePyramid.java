821e3dc4ffd23a46868b0aebb0f15a8deddb3dcd
lcabrerav <luisjoelcabrera@gmail.com>
2018-09-09 23:27:39 -0400

    still working on math

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -5,15 +5,24 @@ public class MakePyramid {
 
         /*   Implement a large Pyramid of stars in the screen with java.
 
-                              *
-                             * *
-                            * * *
-                           * * * *
-                          * * * * *
-                         * * * * * *
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         * * * * * *
 
-        */
+         */
+        int level = 100;
+        for (int i = 0; i <level; i++) {
+            for (int j = 0; j <level-i; j++) {
+                System.out.print(" ");
+            }
+            for(int k=0;k<=i; k++)
+            System.out.print("* ");
 
+        System.out.println("");
+    }
+    }
 
-        }
-  }
+}
