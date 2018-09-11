0dd1516a97e7a542af15fe9dd421987db853c06e
lcabrerav <luisjoelcabrera@gmail.com>
2018-09-11 17:41:58 -0400

    unit

--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,11 +1,13 @@
 package math.problems;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UnitTestingMath {
     public static void main(String[] args) {
         //Apply Unit testing into each classes and methods in this package.
 
+        Assert.assertEquals(120, 120);
 
 
 
